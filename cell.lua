4730d926e4c2b9dd6ad40ebf12d398d8c39961bc
Joseph Hicks <stoiiciism@gmail.com>
2020-12-11 03:01:15 -0500

    Improved mines and numbers

--- cell.lua
+++ cell.lua
@@ -3,7 +3,8 @@ M.__index = M
 
 function M.new(x, y)
 	local self = setmetatable({}, M)
-	self.image = love.graphics.newImage("assets/cells/block.png")
+	self.value = 0
+	self.image = love.graphics.newImage("assets/cells/0.png")
 	self.x = x
 	self.y = y
 	return self
@@ -13,6 +14,10 @@ function M.draw(self)
 	if self.image ~= nil then
 		love.graphics.draw(self.image, self.x * 32, self.y * 32)
 	end
+end
+
+function M.mousepressed(self, x, y, button)
+	
 end
 
 return M
--- main.lua
+++ main.lua
@@ -1,9 +1,45 @@
 function love.load()
+	math.randomseed(os.time())
+
 	Cell = require "cell"
+	
 	cells = {}
 	for y = 1, 9 do
 		cells[y] = {}
 		for x = 1, 9 do cells[y][x] = Cell.new(x - 1, y - 1) end
+	end
+	
+	local m = 10
+	while m > 0 do
+		local x = math.floor(math.random() * 9) + 1
+		local y = math.floor(math.random() * 9) + 1
+		if cells[y][x].value ~= "mine" then
+			cells[y][x].value = "mine"
+			m = m - 1
+		end
+	end
+	
+	for y = 1, 9 do
+		for x = 1, 9 do
+			if cells[y][x].value ~= "mine" then
+				local t = 0
+				if y > 1 and cells[y - 1][x].value == "mine" then t = t + 1 end
+				if y < 9 and cells[y + 1][x].value == "mine" then t = t + 1 end
+				if x > 1 and cells[y][x - 1].value == "mine" then t = t + 1 end
+				if x < 9 and cells[y][x + 1].value == "mine" then t = t + 1 end
+				if y > 1 and x > 1 and cells[y - 1][x - 1].value == "mine" then t = t + 1 end
+				if y < 9 and x > 1 and cells[y + 1][x - 1].value == "mine" then t = t + 1 end
+				if y > 1 and x < 9 and cells[y - 1][x + 1].value == "mine" then t = t + 1 end
+				if y < 9 and x < 9 and cells[y + 1][x + 1].value == "mine" then t = t + 1 end
+				cells[y][x].value = t
+			end
+		end
+	end
+	
+	for y = 1, 9 do
+		for x = 1, 9 do
+			cells[y][x].image = love.graphics.newImage("assets/cells/"..cells[y][x].value..".png")
+		end
 	end
 end
 
